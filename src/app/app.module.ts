0290e7d78bf9b00d759b049691bbaa3098f5fb4d
Andres lomeli <andre.lomeli3@gmail.com>
2020-03-04 20:04:22 -0600

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PaseSalidaComponent } from './pase-salida/pase-salida.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { ServiceService } from './service/service.service';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaseSalidaComponent
+    PaseSalidaComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule, 
+    FormsModule
   ],
   providers: [ServiceService],
   bootstrap: [AppComponent]
