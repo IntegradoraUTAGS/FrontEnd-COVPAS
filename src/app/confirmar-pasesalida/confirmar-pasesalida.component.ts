09345f255911558e40ef59ba5a90992ab0acf9fc
AlexHM <alex13pks@gmail.com>
2020-03-26 22:39:52 -0600

    Agregado un componente de confirmacion

---
+++ src/app/confirmar-pasesalida/confirmar-pasesalida.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmar-pasesalida',
+  templateUrl: './confirmar-pasesalida.component.html',
+  styleUrls: ['./confirmar-pasesalida.component.css']
+})
+export class ConfirmarPasesalidaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
