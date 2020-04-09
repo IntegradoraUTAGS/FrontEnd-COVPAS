05e1d1460b6d9ea8544f6bc004783a9fd7f79f4f
David1360Regalado <josedavidregalado0@gmail.com>
2020-04-09 15:58:11 -0500

    Se integro vigilancia

--- src/app/vigilancia/vigilancia.component.ts
+++ src/app/vigilancia/vigilancia.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ServiceService} from '../service/service.service'
 @Component({
   selector: 'app-vigilancia',
   templateUrl: './vigilancia.component.html',
   styleUrls: ['./vigilancia.component.css']
 })
 export class VigilanciaComponent implements OnInit {
-
-  constructor() { }
+  pase = {}
+  idPase= ""
+  observacion =""
+  kilometrosSalida=""
+  gasolinaSalida=""
+  constructor(protected service:ServiceService) { }
 
   ngOnInit(): void {
   }
   terminarps() {
     
   }
+  obtener(id:any){
+
+    this.service.obtenerPaseSalidaPorId(id).then((data)=>{
+      this.pase = data['pase']
+      console.log(this.pase)
+    }).catch((err)=>{
+      console.error(err)
+    })
+  }
+
+registrar(paseSalida,observacion,nombreReviso,kilometrosSalida,gasolinaSalida){
+  this.service.registrarVigilancia(paseSalida,observacion,nombreReviso,kilometrosSalida,gasolinaSalida).then((data)=>{
+    console.log(data)
+    alert(data['msg'])
+  }).catch((err)=>{
+    console.log(err)
+    alert("No se pudo registrar")
+  })
+}
 
 }
